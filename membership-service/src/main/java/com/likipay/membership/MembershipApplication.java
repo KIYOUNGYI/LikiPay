258f5162eb620a216d422fd51d9d2928e9dc69e7
liki <>
2023-07-09 21:18:12 +0900

    공부내용 여기까지 한 거 반영

--- membership-service/src/main/java/com/likipay/membership/MembershipApplication.java
+++ membership-service/src/main/java/com/likipay/membership/MembershipApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class MembershipApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MembershipApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MembershipApplication.class, args);
+  }
 }
